39aa77e97710198cf93d872ce7dd4177afd8a7a6
Ana Suvac <anasuvac@yahoo.com>
2020-01-18 23:11:44 -0500

    repushing

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO department (dept_name) VALUES ('Back of House'), ('Front of House'), ('Management');
 INSERT INTO company_role (title, salary, dept_id) VALUES
-('General Manager', 70000.00, 3), 
+('General Manager', 80000.00, 3), 
 ('Head Chef', 60000.00, 1),
-('Sous Chef', 50000.00, 1),
+('Sous Chef', 30000.00, 1),
 ('Cook', 30000.00, 1 ),
 ('Dishwasher', 20000.00, 1),                
 ('Front of House Manager', 40000.00, 2),
@@ -10,6 +10,6 @@ INSERT INTO company_role (title, salary, dept_id) VALUES
 ('Server', 20000.00, 1);
 
 INSERT INTO employees (first_name, last_name, emp_role_id, manager_id) VALUES
-('John', 'Smith', 1, null),
-('Jane', 'Smith', 6, 1),
+('Mark', 'Danzino', 1, null),
+('Abigail', 'Jenner', 6, 1),
 ('Derek', 'Fisher', 2, null);
